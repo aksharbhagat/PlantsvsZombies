6e6b2cd5eed30108f3eb85ef5139ded3ff745f0a
JuicyJuJuice <49919689+JuicyJuJuice@users.noreply.github.com>
2019-04-23 22:32:11 -0700

    Update Peashooter.java

--- Peashooter.java
+++ Peashooter.java
@@ -1,7 +1,12 @@
 
-public class Peashooter extends Plant {
+import java.util.ArrayList;
 
-	public Peashooter() {
+
+public class Peashooter extends Plant{
+	private ArrayList<Bullet> Bullets = new ArrayList<Bullet>();
+	public Peashooter(int x, int y) {
+		super(x, y, 100, "peashooter");
+		
 		// TODO Auto-generated constructor stub
 	}
 
